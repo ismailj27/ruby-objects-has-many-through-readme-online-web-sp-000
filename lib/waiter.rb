5b0f4fc5050a94735d48647cdc5b885fffac3df3
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-20 03:52:21 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,5 +14,28 @@ class Waiter
   def self.all
     @@all
   end
+  
+  def new_meal(a_customer, a_total, a_tip)
+    Meal.new(a_customer, self, a_total, a_tip)
+  end
+  
+  def meals
+    meals_arr = []
+    Waiter.all.each do |d_waiter|
+      binding.pry
+      if d_waiter.name == self.name
+        meals_arr << d_waiter
+      end
+    end
+    meals_arr
+  end
+  
+   def best_tipper
+    Customer.all.each do |d_customer|
+      meals.each do |d_meal|
+      end 
+    end
+  end
+  
  
 end
